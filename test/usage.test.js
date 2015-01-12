1404cbfe2f17d1c682e62fa875ed13b114e3e757
Jack Boberg <jack.boberg@gmail.com>
2015-01-12 09:26:30 -0500

    Add tests, and refactor based on testing
    fix notice formatting
    accept optional callbacks everywhere
    update collector endpoint, port
    default buffer to 0
    attempt to connect when creating client

--- test/usage.test.js
+++ test/usage.test.js
@@ -1,11 +1,39 @@
 var test = require('tape');
+
+var server = require('./mock-server');
 var instrumental = require('..');
 
-var client;
-var config = { apiKey: 'secret' };
+var client = instrumental.createClient('secret');
 
-test('create a client', function (t) {
+var metric;
+
+test('creating a client returns a singleton', function (t) {
   t.plan(1);
-  client = instrumental.createClient(config);
-  t.equal(typeof client, 'object');
+
+  var client2 = instrumental.createClient('ignored');
+  t.equal(client, client2);
 });
+
+test('handshake and authentication are automatic', function (t) {
+  t.plan(2);
+
+  client.flush(function () {
+    t.equal(server.data[0].indexOf('hello'), 0);
+    t.equal(server.data[1].indexOf('authenticate'), 0);
+  });
+});
+
+test('client automatically reconnects', function (t) {
+  t.plan(1);
+
+  server.connection.end();
+
+  // FIXME there is a timing issue here
+  setTimeout(function () {
+    client.gauge('metric.reconnect', 1, function () {
+      metric = server.data.pop();
+      t.not(metric.indexOf('metric.reconnect'), -1);
+    });
+  }, 0);
+});
+
